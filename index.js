cbc0d87eb4e366221954da5ffbe2b13f82f74405
Shailbrox <117496630+Shailbrox@users.noreply.github.com>
2024-02-12 01:30:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello Mounika and Mitul!')
+  res.send('Hello Shailendra and Vivek!')
 })
 
 app.listen(port, () => {
